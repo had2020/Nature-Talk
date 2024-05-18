1affe66cefbf0f5d2b4c6c3c602cebde4cdd2f87
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-05-18 16:45:47 -0700

    home page fix

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # rendering page templates
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('main.html')
 
 @app.route('/chat')
 def chat():
