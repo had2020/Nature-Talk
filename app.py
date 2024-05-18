5c1974f6ffc4c302a54c2efae3a3e752efc8cffe
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-05-18 14:56:11 -0700

    changes from 2:56 PM

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# rendering page templates
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -11,16 +12,29 @@ def chat():
    return render_template('chat.html')
 
 @app.route('/mission')
-def chat():
+def mission():
    return render_template('mission.html')
 
 @app.route('/qna')
-def chat():
+def qna():
    return render_template('qna.html')
 
 @app.route('/about')
-def chat():
+def about():
    return render_template('about.html')
+
+@app.route('/accounts')
+def accounts():
+   return render_template('account.html')
+
+@app.route('/signed_in')
+def signed_in():
+   return render_template('signed_in.html')
+
+# chat page methods
+@app.route('/logined')
+def logined():
+   return render_template('signed_in.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
