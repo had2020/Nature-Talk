1f1a292050b3cbd2efc1c0b3a9e1a62c868f9fc9
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-05-18 12:29:32 -0700

    adding chat html and nav bar on index

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/chat')
+def chat():
+   return render_template('chat.html')
+
 if __name__ == '__main__':
   app.run(debug=True)
